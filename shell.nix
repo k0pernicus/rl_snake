f192ecc7049b7fa9a0ea423530031ea680744156
k0pernicus <dev@carette.xyz>
2026-04-04 18:27:20 +0200

    Solved a crash in PyGame

--- shell.nix
+++ shell.nix
@@ -1,23 +1,22 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
-  # Tools required for the build process (run on your machine)
   nativeBuildInputs = with pkgs; [
     uv
     pkg-config
   ];
 
-  # Libraries required for Pygame to compile and link against
   buildInputs = with pkgs; [
     sdl2-compat
     SDL2_image
     SDL2_ttf
     SDL2_mixer
+    freetype      # Added the missing dependency for pygame.font
   ];
 
-  # Explicitly tell the compiler and linker where the Nix store paths are
+  # Safely extract headers and libraries without risking missing attributes
   shellHook = ''
-    export CPATH="${pkgs.sdl2-compat.dev}/include/SDL2:$CPATH"
-    export LDFLAGS="-L${pkgs.sdl2-compat}/lib -L${pkgs.SDL2_image}/lib -L${pkgs.SDL2_ttf}/lib -L${pkgs.SDL2_mixer}/lib $LDFLAGS"
+    export CPATH="${pkgs.lib.getDev pkgs.sdl2-compat}/include/SDL2:${pkgs.lib.getDev pkgs.SDL2_ttf}/include/SDL2:${pkgs.lib.getDev pkgs.SDL2_image}/include/SDL2:${pkgs.lib.getDev pkgs.SDL2_mixer}/include/SDL2:${pkgs.lib.getDev pkgs.freetype}/include/freetype2:$CPATH"
+    export LDFLAGS="-L${pkgs.lib.getLib pkgs.sdl2-compat}/lib -L${pkgs.lib.getLib pkgs.SDL2_ttf}/lib -L${pkgs.lib.getLib pkgs.SDL2_image}/lib -L${pkgs.lib.getLib pkgs.SDL2_mixer}/lib -L${pkgs.lib.getLib pkgs.freetype}/lib $LDFLAGS"
   '';
 }
